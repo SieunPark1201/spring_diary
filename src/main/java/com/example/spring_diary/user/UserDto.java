c3c2dfd7fa1fe66244d5af2e5081e7d980b11c9f
Sieun Park <park12010@naver.com>
2024-01-19 10:13:29 +0900

    UserDto, UserService 완성

--- src/main/java/com/example/spring_diary/user/UserDto.java
+++ src/main/java/com/example/spring_diary/user/UserDto.java
@@ -1,10 +1,21 @@
 package com.example.spring_diary.user;
 
 
+import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class UserDto {
+
+    private long userId;
+    private String nickname;
+    private String email;
+    private String password;
+    private LocalDate birthday;
+
+
 }
--- src/main/java/com/example/spring_diary/user/UserService.java
+++ src/main/java/com/example/spring_diary/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.spring_diary.user;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +9,45 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    //회원가입
+    public void createUser(UserDto userDto){
+        User user1 = new User(userDto.getNickname(), userDto.getEmail(),
+                userDto.getPassword(), userDto.getBirthday());
+        userRepository.save(user1);
+    }
+
+
+    //회원수정
+    public void updateUser(UserDto userDto) throws Exception{
+        User user1 = userRepository.findByUserId(userDto.getUserId());
+
+        if (user1 == null) {
+            System.out.println("해당 사용자를 찾을 수 없습니다.");
+            throw new EntityNotFoundException();
+        } else {
+            user1.setNickname(userDto.getNickname());
+            user1.setEmail(userDto.getEmail());
+            user1.setPassword(userDto.getPassword());
+            user1.setBirthday(userDto.getBirthday());
+
+            userRepository.save(user1);
+        }
+
+    }
+
+
+    //회원삭제
+    public void deleteUser(UserDto userDto) throws Exception{
+        User user1 = userRepository.findByUserId(userDto.getUserId());
+
+        if (user1 == null) {
+            System.out.println("해당 사용자를 찾을 수 없습니다.");
+            throw new EntityNotFoundException();
+        } else {
+            userRepository.delete(user1);
+        }
+    }
+
+
 }
