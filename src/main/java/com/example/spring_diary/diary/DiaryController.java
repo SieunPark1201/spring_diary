10b83af33dc55d8faf24b311b60d713883537c8c
Sieun Park <park12010@naver.com>
2024-03-30 15:05:56 +0900

    삭제 MAPPING: GET -> DELETE

--- src/main/java/com/example/spring_diary/diary/DiaryController.java
+++ src/main/java/com/example/spring_diary/diary/DiaryController.java
@@ -2,6 +2,7 @@ package com.example.spring_diary.diary;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -48,7 +49,7 @@ public class DiaryController {
 
 
 //    글 삭제 화면
-    @PostMapping("diary/delete")
+    @DeleteMapping("diary/delete")
     public String deleteDiary(DiaryDto diaryDto){
         diaryService.deleteDiary(diaryDto);
         return "redirect:/";
